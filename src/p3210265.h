08077b2f4cf683632f6ad36b148a2baede751829
Giorgos Athanasopoulos <90207655+GiorgosAthanasopoulos@users.noreply.github.com>
2023-05-12 23:57:21 +0300

    Add files via upload

--- src/p3210265.h
+++ src/p3210265.h
@@ -1,38 +1,40 @@
+typedef unsigned int uint;
+
 // cooks
-int NCook = 2;
+uint NCook = 2;
 // ovens
-int NOven = 15;
+uint NOven = 15;
 // packers
-int NPacker = 2;
+uint NPacker = 2;
 // delivery men
-int NDeliverer = 10;
-// lowest time it takes for a customer to get into the ordering system
-int TOrderlow = 1;
-// highest time it takes for a customer to get into the ordering system
-int TOrderhigh = 3;
+uint NDeliverer = 10;
+// lowest time it takes for a customer to get uinto the ordering system
+uint TOrderlow = 1;
+// highest time it takes for a customer to get uinto the ordering system
+uint TOrderhigh = 3;
 // lowest amount of pizzas in a single order
-int NOrderlow = 1;
+uint NOrderlow = 1;
 // highest amount of pizzas in a single order
-int NORderhigh = 5;
+uint NOrderhigh = 5;
 // possibility of a pizza being plain
-int PPlain = 60;
+uint PPlain = 60;
 // lowest amount of time it takes for the ordering system to charge the customer
-int TPaymentlow = 1;
+uint TPaymentlow = 1;
 // highest amount of time it takes for the ordering system to charge the customer
-int TPaymenthigh = 3;
+uint TPaymenthigh = 3;
 // possibility of the order failing due to failure in payment
-int PFail = 10;
+uint PFail = 10;
 // cost of plain pizza
-int CPlain = 10;
+uint CPlain = 10;
 // cost of special pizza
-int CSpecial = 12;
+uint CSpecial = 12;
 // time it takes for a cook to make the pizza
-int TPrep = 1;
+uint TPrep = 1;
 // time it takes for the oven to bake the pizza
-int TBake = 10;
+uint TBake = 10;
 // time it takes to pack each pizza
-int TPack = 1;
+uint TPack = 1;
 // lowest amount of time it takes for the delivery guy to deliver an order 
-int TDellow = 5;
+uint TDellow = 5;
 // highest amount of time it takes for the delivery guy to deliver an order
-int TDelhigh = 15;
+uint TDelhigh = 15;
